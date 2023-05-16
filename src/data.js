7be161336f97cd9ec229fb720dc57bfe48be023c
c <carolinaposada@Carolinas-MacBook-Pro.local>
2023-05-16 13:09:32 -0500

    Últimos cambios

--- src/data.js
+++ src/data.js
@@ -1,9 +1,41 @@
 // estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+import data from './data/pokemon/pokemon.js';
+export const filtroPokemones = (data, propiedad) => {
+  return data.filter(pokemon => {
+    return pokemon.type === propiedad
+  })
+}
+
+console.log(filtroPokemones(pokemones, 'grass'))
 
 export const anotherExample = () => {
   return 'OMG';
 };
+
+const pokemones = data.pokemon;
+
+//creando el filtro por cada tipo de poder.
+// debemos crear en el HTML una lista para que se vean los tipos de poder
+const grass = pokemones.filter(pokemon => {
+return pokemon.type === 'grass'
+})
+
+const poison = pokemones.filter(pokemon => {
+return pokemon.type === 'poison'
+})
+
+
+
+
+
+//funciones data.js -> creando funnciones que se encargan de manipular la data
+
+//function filtrar(data, propiedad) {
+//return data.filter(pokemon => {
+//return pokemon.type === propiedad
+// })
+//}
+//console.log(filtrar(pokemones, 'type'))
+
+//pokemon.type === propiedad - va a pasar por cada type
--- src/main.js
+++ src/main.js
@@ -1,17 +1,70 @@
-import { example } from './data.js';
+import { filtroPokemones } from './data.js';
 
 import data from './data/pokemon/pokemon.js';
 
+//Primera historia de usuario
 const pokemones = data.pokemon;
-
 const contenedor = document.getElementById('contenedor');
-pokemones.forEach(pokemon => {
-  contenedor.innerHTML += `
-    <div class= "contenedor_pokemones">
-    <h2>${pokemon.num}<h2>
-    <img src="${pokemon.img}">
-    </div>
-    `  
-})
 
-console.log(example, data);
+pokemones.forEach(pokemon => {
+  const img = pokemon.img;
+  const name = pokemon.name;
+  const height = pokemon.size['height'];
+  const weight = pokemon.size['weight'];
+  const type = pokemon.type;
+  const baseCaptureRate = pokemon.encounter['base-capture-rate'];
+
+
+  const card = document.createElement('div');
+  card.className = 'pokemon-card';
+  card.innerHTML = `
+    <img src="${img}" alt="${name}">
+    <h3>${name}</h3> 
+    <div class="cardp">
+        <p>Height: ${height}</p>
+        <p>Weight: ${weight}</p>
+        <p>Base Capture Rate: ${baseCaptureRate}</p>
+        <p>Type: ${type}</p>
+    </div>
+  `;
+
+  contenedor.appendChild(card);
+});
+
+//Historia usuario 2
+const botonFiltrar = document.getElementById('boton-filtrar');
+
+botonFiltrar.addEventListener('click', () => {
+  contenedor.innerHTML = '';
+  pokemones.forEach(grass => {
+  contenedor.innerHTML += `<p>${pokemon.type}</p>`;
+  })
+}
+
+
+  const contenedorBotones = document.getElementById('container_botones');
+  contenedorBotones.style.display = "none";
+  const principalParrafo = document.getElementById('principal_parrafo');
+  principalParrafo.style.display = "none";
+
+  
+  //const filtroPokemones = data.filtroPokemones;
+  //contenedor.innerHTML += `<p>${filtroPokemones}</p>`;
+
+
+  
+
+  //¿¿¿¿aqui iria la lista desplegable????
+  //<select name="filtro-type">
+  //<option value="value1">grass</option>
+  //<option value="value2">poison</option>
+  //<option value="value3">fire</option>
+  //</select>
+
+
+
+});
+
+
+
+
